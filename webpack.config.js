5cafdb3ca09b88d51a4ac4ca5d6b9e1a2b4c4156
pancakeCaptain <jaaulner@gmail.com>
2016-03-10 20:19:08 -0600

    Add webpack dev server with hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,18 @@ var path = require('path'),
     HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/main.js'),
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        path.resolve(__dirname, 'src/main.js')
+    ],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack Tutorial',
+            title: 'Test Scaffolder',
             filename: 'index.html'
         })
     ],
