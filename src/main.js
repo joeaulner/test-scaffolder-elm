d61d5392369d556e8455f5aa8819ba2aa9a6f45a
pancakeCaptain <jaaulner@gmail.com>
2016-03-12 17:54:14 -0600

    Require jQuery for materialize interactions

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 require('../node_modules/materialize-css/dist/css/materialize.min.css');
+require('../node_modules/materialize-css/dist/js/materialize.min.js');
 var Elm = require('./app/Main.elm');
 
 Elm.fullscreen(Elm.Main);
