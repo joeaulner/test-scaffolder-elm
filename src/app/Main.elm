ebea878276dbbed5d95408dede17b7cfb4950af3
pancakeCaptain <jaaulner@gmail.com>
2016-03-12 17:19:06 -0600

    Move application logic into separate model
    Main should only be used as an entry point into the app or to communicate
    with JS ports

--- src/app/Main.elm
+++ src/app/Main.elm
@@ -1,22 +1,9 @@
 module Main where
 
 import Html exposing (Html)
-import Html.Attributes as Attr
+import TestScaffolder
 
 
 main : Html
 main =
-    Html.div []
-        [ Html.nav
-            [ Attr.class "light-green darken-1" ]
-            [ Html.div
-                [ Attr.class "nav-wrapper container" ]
-                [ Html.span
-                    [ Attr.class "brand-logo" ]
-                    [ Html.text "Test Scaffolder" ]
-                ]
-            ]
-        , Html.div
-            [ Attr.class "container" ]
-            [ Html.div [] [ Html.text "<content here>" ] ]
-        ]
+    TestScaffolder.view
