fb1d4bc8aef6b07df6e127010bcd5e56c71634f3
pancakeCaptain <jaaulner@gmail.com>
2016-04-05 14:12:49 -0500

    Refactor and complete lexer

--- src/app/Lexer.elm
+++ src/app/Lexer.elm
@@ -2,7 +2,7 @@ module Lexer (Token, toTokens) where
 
 import String
 import Maybe
-import Regex
+import Regex exposing (Regex)
 
 -- REMOVE
 import Debug
@@ -13,6 +13,14 @@ type Token
     = Indent
     | Dedent
     | Samedent
+    | Block BlockType
+    | Description String
+    
+
+type BlockType
+    = Feature
+    | Test
+    | NoBlock
 
 
 toTokens : String -> List Token
@@ -21,8 +29,7 @@ toTokens input =
         lines = String.lines input
         (_, linesTks) = tokenizeLines [] lines
     in
-        Debug.log "tokenized"
-            (List.foldr (++) [] linesTks)
+        Debug.log "output" (List.foldr (++) [] linesTks)
 
 
 tokenizeLines : List Int -> List String -> (List Int, List (List Token))
@@ -34,50 +41,71 @@ tokenizeLines indentStack lines =
                 (indentStack', headTks) = tokenizeLine indentStack hd
                 (indentStack'', tailTks) = tokenizeLines indentStack' tl
             in
-                (indentStack'', [ headTks ] ++ tailTks)
+                (indentStack'', headTks :: tailTks)
 
 
 tokenizeLine : List Int -> String -> (List Int, List Token)
 tokenizeLine indentStack str =
     let
-        matchedStr = Debug.log "test regex" (match str)
-        (indentCt, indentTk) = tokenizeIndent indentStack str
+        (indentStack', indentToken, str') = tokenizeIndent indentStack str
+        (typeToken, str'') = tokenizeType str'
+        descToken = tokenizeDesc str''
+    in
+        (indentStack', [indentToken, typeToken, descToken])
+
+
+tokenizeIndent : List Int -> String -> (List Int, Token, String)
+tokenizeIndent indentStack str =
+    let
+        matched = find "^\\s*" str
+        indentCt = String.length matched
+        str' = String.dropLeft indentCt str
+        indentTk =
+            let
+                prevIndentCt = List.head indentStack |> Maybe.withDefault 0
+            in
+                if indentCt > prevIndentCt then Indent
+                else if indentCt < prevIndentCt then Dedent
+                else Samedent
         indentStack' =
             case indentTk of
                 Indent -> indentCt :: indentStack
                 Dedent -> Maybe.withDefault [] (List.tail indentStack)
-                Samedent -> indentStack
+                _ -> indentStack
     in
-        (indentStack', [ indentTk ])
+        (indentStack', indentTk, str')
 
 
-match : String -> List (Maybe String)
-match str =
+tokenizeType : String -> (Token, String)
+tokenizeType str =
     let
-        regex = Regex.regex "^(\\s*)(feature|test)\\s*:\\s*(\\w.*)"
-            |> Regex.caseInsensitive
+        feature = Regex.regex "^feature:\\s*"
+        test = Regex.regex "^test:\\s*"
+        trim n s = String.dropLeft n s |> String.trimLeft
+        (blockType, str') =
+            if contains "^feature:\\s*" str then
+                (Feature, trim 8 str)
+            else if contains "^test:\\s*" str then
+                (Test, trim 5 str)
+            else
+                (NoBlock, str)
     in
-        Regex.find (Regex.All) regex str
-            |> List.map .submatches
-            |> List.head
-            |> Maybe.withDefault []
+        (Block blockType, str')
 
 
-tokenizeIndent : List Int -> String -> (Int, Token)
-tokenizeIndent indentStack str =
-    let
-        prevIndentCt = Maybe.withDefault 0 (List.head indentStack)
-        indentCt = countSpaces str
-        indentTk =
-            if indentCt > prevIndentCt then Indent
-            else if indentCt < prevIndentCt then Dedent
-            else Samedent
-    in
-        (indentCt, indentTk)
+tokenizeDesc : String -> Token
+tokenizeDesc str =
+    Description <| String.trim str
 
 
-countSpaces : String -> Int
-countSpaces s =
-    case Maybe.withDefault ('.', "") (String.uncons s) of
-        (' ', ss) -> 1 + countSpaces ss
-        _ -> 0
+contains : String -> String -> Bool
+contains regexStr str =
+    Regex.contains (Regex.regex regexStr) str
+
+
+find : String -> String -> String
+find regexStr str =
+    Regex.find (Regex.AtMost 1) (Regex.regex regexStr) str
+        |> List.map .match
+        |> List.head
+        |> Maybe.withDefault ""
